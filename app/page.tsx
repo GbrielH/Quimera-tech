e3a2d68ebd9665ed5fc5cdfab06139769ef9b15e
GbrieIH <Henrique0limah@gmail.com>
2026-07-28 20:51:37 -0300

    Adiciona logo do WhatsApp

--- app/page.tsx
+++ app/page.tsx
@@ -635,7 +635,12 @@ export default function Home() {
         rel="noreferrer"
         aria-label="Falar com a Quimera Tech no WhatsApp"
       >
-        <span>✆</span>
+        <span aria-hidden="true">
+          <svg viewBox="0 0 24 24" role="img">
+            <path d="M20.5 11.6a8.5 8.5 0 0 1-12.6 7.5L3 20.5l1.3-4.7a8.5 8.5 0 1 1 16.2-4.2Z" />
+            <path d="M8.1 7.4c.2-.4.4-.4.7-.4h.5c.2 0 .4.1.5.5l.8 1.9c.1.3.1.5-.1.7l-.6.8c-.2.2-.1.4 0 .6.7 1.2 1.6 2.1 2.8 2.7.2.1.4.1.6-.1l.8-1c.2-.2.4-.3.7-.2l2 .9c.3.1.5.3.5.5 0 .2-.1 1.2-.6 1.8-.5.6-1.4.9-2.2.9-.6 0-1.4-.2-2.4-.6a10.7 10.7 0 0 1-5.5-5c-.5-1-.7-1.8-.7-2.4 0-.6.2-1.2.6-1.6.2-.2.4-.4.6-.4Z" />
+          </svg>
+        </span>
         <i>WhatsApp</i>
       </a>
     </main>
